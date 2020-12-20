7de06109145e81138391faae8e62ab92c1878daa
Aditya <rajaditya27145@gmail.com>
2020-12-20 11:28:42 +0530

    fixing content

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,45 +4,43 @@ import "./about.css";
 // import polygon from '../../media/polygonBlue.png'
 
 export class About extends Component {
-    render() {
-        return (
-            <section id='about'>
-                <div className='section-break'>
-                    <h1>About Me</h1>
-                </div>
-                <div className='section-fill-about'>
-                    <div id='about-content'>
-                        <p>
-                            Hello! I'm Aditya Raj, a Fullstack Web Developer
-                            based in Bangalore, India <br />
-                            who loves to build scalable Web Apps with efficient
-                            backends and <br />
-                            clean User Interface using the latest web
-                            technologies.
-                        </p>
-                        <p>
-                            After dropping out of college, I dived into Web
-                            Development and <br />
-                            have been working on cool projects with amazing
-                            people since then.
-                        </p>
-                        <p>Here are a few technologies I'm proficient in:</p>
-                        <ul>
-                            <li>HTML {"&"} (S)CSS</li>
-                            <li>JavaScript (ES6+)</li>
-                            <li>React.js</li>
-                            <li>Node.js</li>
-                            <li>MongoDB</li>
-                            <li>Express</li>
-                        </ul>
-                    </div>
-                    <div id='about-img'>
-                        <img src={myPic} alt='The author' />
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section id="about">
+        <div className="section-break">
+          <h1>About Me</h1>
+        </div>
+        <div className="section-fill-about">
+          <div id="about-content">
+            <p>
+              Hello! I'm Aditya Raj, a Fullstack Web Developer based in
+              Bangalore, India <br />
+              with a love and passion for creating beautiful and smooth <br />{" "}
+              User Experiences using the latest web technologies.
+            </p>
+            <p>
+              After dropping out of college, I dived into Web Development and{" "}
+              <br />
+              have been working on real-world projects with amazing people since
+              then.
+            </p>
+            <p>Here are a few technologies I'm proficient in:</p>
+            <ul>
+              <li>HTML {"&"} (S)CSS</li>
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+              <li>React.js</li>
+              <li>Node.js</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+          <div id="about-img">
+            <img src={myPic} alt="The author" />
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default About;
